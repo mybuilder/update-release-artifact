e8903b987716b69392af17923545bbd5cb488719
Tom Cant <tomcant@gmail.com>
2021-09-27 17:05:32 +0100

    Accept a path to a locally stored artifact instead of relying on the artifact attached to a workflow run

--- index.js
+++ index.js
@@ -1,83 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 const core = require('@actions/core');
 const { Octokit } = require('@octokit/action');
-const AdmZip = require('adm-zip');
 
-const sleep = seconds =>
-  new Promise(r => setTimeout(r, seconds * 1000));
-
-const findWorkflowRunArtifactId = async (
-  workflow,
-  workflowCommit,
-  artifactName,
-  pollingAttempts = 60
-) => {
-  const octokit = new Octokit();
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-
-  if (pollingAttempts < 1) {
-    throw new Error(
-      'Failed to find workflow run... exceeded attempts'
-    );
-  }
-
-  for await (const runs of octokit.paginate.iterator(
-    octokit.actions.listWorkflowRuns,
-    {
-      owner,
-      repo,
-      workflow_id: workflow,
-    }
-  )) {
-    const run = runs.data.find(r => r.head_sha === workflowCommit && r.conclusion === 'success');
-    if (run) {
-      const artifacts =
-        await octokit.actions.listWorkflowRunArtifacts({
-          owner,
-          repo,
-          run_id: run.id,
-        });
-      const artifact = artifacts.data.artifacts.find(
-        a => a.name === artifactName
-      );
-      if (artifact) return artifact.id;
-    }
-  }
-
-  await sleep(1);
-
-  console.log('- Failed to find workflow run... retrying');
-
-  return await findWorkflowRunArtifactId(
-    workflow,
-    workflowCommit,
-    artifactName,
-    pollingAttempts - 1
-  );
-};
-
-const pullWorkflowArtifact = async (artifactId, artifactName) => {
-  const octokit = new Octokit();
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-
-  const zip = await octokit.actions.downloadArtifact({
-    owner,
-    repo,
-    artifact_id: artifactId,
-    archive_format: 'zip',
-  });
-
-  const adm = new AdmZip(Buffer.from(zip.data));
-  adm.extractAllTo('artifact', true);
-
-  return `./artifact/${artifactName}`;
-};
-
-const updateReleaseArtifact = async (
-  releaseId,
-  artifactName,
-  workflowArtifactLocalPath
-) => {
+const updateReleaseArtifact = async (releaseId, artifactPath) => {
   const octokit = new Octokit();
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 
@@ -106,44 +32,21 @@ const updateReleaseArtifact = async (
     owner,
     repo,
     release_id: releaseId,
-    name: artifactName,
-    data: fs.readFileSync(workflowArtifactLocalPath),
+    name: path.basename(artifactPath),
+    data: fs.readFileSync(artifactPath),
   });
 };
 
 async function run() {
   try {
-    const workflow = core.getInput('workflow');
-    const workflowCommit = core.getInput('workflow_commit');
-    const artifactName = core.getInput('artifact_name');
+    const artifactPath = core.getInput('artifact_path');
     const releaseId = core.getInput('release_id');
 
-    console.log(
-      `workflow: ${workflow}, workflowCommit: ${workflowCommit}, artifactName: ${artifactName}, releaseId: ${releaseId}`
-    );
-
-    console.log(`Finding workflow run artifact`);
-
-    const workflowArtifactId = await findWorkflowRunArtifactId(
-      workflow,
-      workflowCommit,
-      artifactName
-    );
-
-    console.log(`Pulling workflow run artifact`);
-
-    const workflowArtifactLocalPath = await pullWorkflowArtifact(
-      workflowArtifactId,
-      artifactName
-    );
+    console.log(`artifactPath: ${artifactPath}, releaseId: ${releaseId}`);
 
     console.log(`Updating release artifact`);
 
-    await updateReleaseArtifact(
-      releaseId,
-      artifactName,
-      workflowArtifactLocalPath
-    );
+    await updateReleaseArtifact(releaseId, artifactPath);
 
     console.log(`Successfully updated release artifact`);
   } catch (error) {
